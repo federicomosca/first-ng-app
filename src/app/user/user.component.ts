dd52a6e68a08037c3fa632d4861ab3bfda59256e
Federico Mosca <f.mosca@xrmhub.it>
2024-10-31 10:33:30 +0100

    aggiunto un listener per il click del button nell'user component e un metodo da invocare a ogni click

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
+const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -18,6 +18,7 @@ export class UserComponent {
   }
 
   onSelectUser(){
-    console.log('Clicked!');
+const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
+this.selectedUser = DUMMY_USERS[randomIndex];
   }
 }
