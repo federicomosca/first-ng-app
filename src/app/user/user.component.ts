8525746977f8622d3ab9d95cbab1260aea264589
Federico Mosca <f.mosca@xrmhub.it>
2024-11-04 10:01:50 +0100

    Output decorator o output method

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,6 +4,7 @@ import {
   EventEmitter,
   Input,
   Output,
+  output,
 } from '@angular/core';
 
 @Component({
@@ -17,6 +18,7 @@ export class UserComponent {
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
   @Output() select = new EventEmitter();
+  // select = output<string>(); //sintassi possibile ma relativamente nuova
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
