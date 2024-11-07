8b9d1ff590426e7d355e0f8ea4020b61e9f5abf1
Federico Mosca <f.mosca@xrmhub.it>
2024-11-07 09:32:16 +0100

    aggiungo highlight su utente selezionato e implemento funzionamento del tasto 'complete' su una task

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -16,6 +16,7 @@ import { type User } from './user.model';
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
   // select = output<string>(); //sintassi possibile ma relativamente nuova
 
